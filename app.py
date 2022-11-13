12e313f251b88fef79d6f17dd61ac5e21dc79440
econmari <118018702+econmari@users.noreply.github.com>
2022-11-13 22:43:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -94,7 +94,6 @@ with col1:
         X = pd.DataFrame([[Credit_Mix, Outstanding_Debt, Interest_Rate, Changed_Credit_Limit, Total_Months_Credit_History_Age, Num_Credit_Card, Delay_from_due_date, Num_of_Loan, Num_Bank_Accounts, Monthly_Balance]], 
                          columns = ["Credit_Mix", "Outstanding_Debt", "Interest_Rate", "Changed_Credit_Limit", "Total_Months_Credit_History_Age", "Num_Credit_Card", "Delay_from_due_date", "Num_of_Loan", "Num_Bank_Accounts", "Monthly_Balance"])
         X = X.replace(["Negative", "Neutral", "Positive"], [1, 2, 3])
-        X = pd.DataFrame(scaler.transform(X),columns = X.columns)
 
 
         # Get prediction
