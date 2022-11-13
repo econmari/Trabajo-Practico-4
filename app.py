06ad3a175b087fbf78dd08fed49f16d76b4099b0
econmari <118018702+econmari@users.noreply.github.com>
2022-11-13 17:12:16 +0100

    Update app.py

--- app.py
+++ app.py
@@ -91,8 +91,8 @@ with col1:
         # Store inputs into dataframe
         scaler = StandardScaler()
 
-        X = pd.DataFrame([[Credit_Mix, Outstanding_Debt, Interest_Rate, Num_Credit_Inquiries, Changed_Credit_Limit]], 
-                         columns = ["Credit_Mix", "Outstanding_Debt", "Interest_Rate", "Changed_Credit_Limit", "otal_Months_Credit_History_Age", "Num_Credit_Card", "Delay_from_due_date", "Num_of_Loan", "Num_Bank_Accounts", "Monthly_Balance"])
+        X = pd.DataFrame([[Credit_Mix, Outstanding_Debt, Interest_Rate, Changed_Credit_Limit, Total_Months_Credit_History_Age, Num_Credit_Card, Delay_from_due_date, Num_of_Loan, Num_Bank_Accounts, Monthly_Balance]], 
+                         columns = ["Credit_Mix", "Outstanding_Debt", "Interest_Rate", "Changed_Credit_Limit", "Total_Months_Credit_History_Age", "Num_Credit_Card", "Delay_from_due_date", "Num_of_Loan", "Num_Bank_Accounts", "Monthly_Balance"])
         X = X.replace(["Negative", "Neutral", "Positive"], [1, 2, 3])
         
         X = pd.DataFrame(scaler.fit_transform(X),columns = X.columns)
